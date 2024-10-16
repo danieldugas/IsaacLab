be21f5c4f6440695621fbbb42e8de00c8555db35
Daniel Dugas <daniel@dugas.ch>
2024-10-16 13:53:49 +0200

    default wandb project to loco-isaaclab

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/utils/wrappers/rsl_rl/rl_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/utils/wrappers/rsl_rl/rl_cfg.py
@@ -126,7 +126,7 @@ class RslRlOnPolicyRunnerCfg:
     neptune_project: str = "isaaclab"
     """The neptune project name. Default is "isaaclab"."""
 
-    wandb_project: str = "isaaclab"
+    wandb_project: str = "loco-isaaclab"
     """The wandb project name. Default is "isaaclab"."""
 
     ##
